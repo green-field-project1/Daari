01c9963e707c472c4fa2494aae1d3848f847efc6
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-03 15:08:06 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Home/>
+      </div>
+    ),
+  },
+  {
+    path:"/about",
+    element:(
+      <div>
+        <About/>
+      </div>
+    )
+  },
+  {
+    path:"/profile",
+    element:(
+      <div>
+        <Profile/>
+      </div>
+    )
+  },
+  {
+    path:"/sign-in",
+    element:(
+      <div>
+        <Signin/>
+      </div>
+    )
+  },
+  {
+    path:"/sign-out",
+    element:(
+      <div>
+        <SignOut/>
+      </div>
+    )
+  }
+
+]);
 
 const App = () => {
-  return (
-    <div className='text-red-500'>App</div>
-  )
-}
+  return <RouterProvider router={router}/>
+};
 
-export default App
+export default App;
