313816f827f5b1ac5b9bd903047de405099ff89d
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-04 23:27:44 +0100

    add google authentication

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4BPOdYySb17wnqVcg6IItGmhID85frZA",
+  authDomain: "daari-7a945.firebaseapp.com",
+  projectId: "daari-7a945",
+  storageBucket: "daari-7a945.appspot.com",
+  messagingSenderId: "152465947995",
+  appId: "1:152465947995:web:8613b63989e53ebdb0ae7e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 
 const Signup = () => {
@@ -71,6 +72,7 @@ const Signup = () => {
         >
           {loading ? "Loading..." : "Sign Up"}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account ?</p>
