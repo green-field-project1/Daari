e227f5f1f4886c6494ee65a5d5efb9d8f5b6a3e1
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-04 13:55:07 +0100

    create sign in route and jwt authentification

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 const User = require('../models/user.model')
 const bcryptjs = require('bcryptjs')
+const { errorHandler } = require('../utils/error.js')
+const jwt = require('jsonwebtoken')
+const signup = async (req, res, next) => {
 
-const signup = async (req,res,next)=>{
-
-    const {username,email,password} = req.body
-    const hashedPassword = bcryptjs.hashSync(password,10)
-    const newUser = new User({username,email,password:hashedPassword})
+    const { username, email, password } = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword })
     try {
         await newUser.save()
         res.status(201).json('user created successfully !')
@@ -14,4 +15,22 @@ const signup = async (req,res,next)=>{
     }
 }
 
+const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not Found !'))
+        validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, 'Wrong cridentials !'))
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc
+            res.cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 1000) })
+                .status(200)
+                .json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports.signup = signup
+module.exports.signin = signin
