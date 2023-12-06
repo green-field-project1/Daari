9073b28fbd94e28bedaf18bd87c0be95c778c27b
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-06 01:02:50 +0100

    Complete update user functionality and fix server error

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,32 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser:null,
-    error:null,
-    loading:false
+    currentUser: null,
+    error: null,
+    loading: false
 }
 
 const userSlice = createSlice({
-    name:'user',
+    name: 'user',
     initialState,
-    reducers:{
-        signInStart:(state)=>{
+    reducers: {
+        signInStart: (state) => {
             state.loading = true
         },
-        signInSuccess:(state,action)=>{
-           state.currentUser = action.payload
-           state.loading = false
-           state.error = null 
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload
+            state.loading = false
+            state.error = null
         },
-        signInFailure: (state,action)=>{
+        signInFailure: (state, action) => {
             state.error = action.payload
             state.loading = false
 
+        },
+        updateUserStart: (state, action) => {
+            state.loading = true
+
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload
+            state.loading = false
+            state.error = null
+        },
+        updateUserFailure: (state, action) => {
+            state.error = action.payload
+            state.loading = false
         }
-        
-    }    
+
+    }
 })
 
-export const {signInStart,signInSuccess,signInFailure} = userSlice.actions
+export const {
+    signInStart,
+    signInSuccess,
+    signInFailure,
+    updateUserStart,
+    updateUserSuccess,
+    updateUserFailure } = userSlice.actions
 
 export default userSlice.reducer
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,22 +3,27 @@ const { errorHandler } = require("../utils/error.js")
 const bcryptjs = require("bcryptjs")
 
 
-const updateUser = async (req,res,next)=>{
-    if(req.user.id !== req.params.id) return next(errorHandler(401,'you are not allowed to update this user'))
+const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'you are not allowed to update this user'))
     try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password,10)
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
         }
-        const updated = await User.findByIdAndUpdate(req.params.id,req,{
-            $set:{
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar
-            }
-        },{new:true})
 
-        const {password,...rest} = updated._doc
+        const updateObject = {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+        };
+
+        const updated = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: updateObject },
+            { new: true }
+        );
+
+        const { password, ...rest } = updated._doc
         res.status(201).json(rest)
     } catch (error) {
         next(error)
