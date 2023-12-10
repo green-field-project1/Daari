74b64c7ecedf1821518debca927b1a71a71aac57
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-10 11:49:46 +0100

    create search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import CreateListing from "./pages/CreateListing";
 import PrivateRoute from "./components/PrivateRoute";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -103,6 +104,15 @@ const router = createBrowserRouter([
       </div>
     ),
   },
+  {
+    path: "/search",
+    element: (
+      <div>
+        <Header />
+        <Search />
+      </div>
+    ),
+  }
 ]);
 
 const App = () => {
