e8b492eccbaf7cc635eef8747415790f9acbba32
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-11 09:00:17 +0100

    create about page

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -6,7 +6,6 @@ const verifyToken = (req, res, next) => {
     if (!token) return next(errorHandler(401, 'Unauthorized'))
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return next(errorHandler(403, 'Token is invalid'))
-
         req.user = user
         next()
     })
